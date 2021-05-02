3a4483fb69d20873e89c36cbbc56289886d38827
Liz Town <liztown@gmail.com>
2021-05-02 11:10:54 -0500

    replacing BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
